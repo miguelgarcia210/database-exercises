d856d4cd9bbbb4f26e7fd028c679cfc6c8b8458f
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-12-18 19:15:17 -0600

    exercise completed

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,9 +1,19 @@
 USE codeup_test_db;
 
-/*
- Albums released after 1991
-Albums with the genre 'disco'
-Albums by 'Whitney Houston' (...or maybe an artist of your choice)
- */
+SELECT '--------- ALBUMS AFTER 1991' AS '';
+SELECT * FROM albums WHERE release_date > 1991;
+SELECT '--------- DISCO GENRE ALBUMS' AS '';
+SELECT * FROM albums WHERE genre LIKE 'disco%';
+SELECT '--------- ALBUMS FEATURING WHITNEY HOUSTON' AS '';
+SELECT * FROM albums WHERE artist LIKE 'whitney houston%';
 
- SELECT * FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE genre LIKE 'disco%';
+DELETE FROM albums WHERE artist LIKE 'whitney houston%';
+
+-- reset id's in ascending order
+# SET @num := 0;
+# UPDATE albums SET id = @num := (@num+1);
+# ALTER TABLE albums AUTO_INCREMENT = 1;
+
+SELECT * FROM albums;
