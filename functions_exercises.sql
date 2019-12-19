44563adab53d23c288ebbd7935001cb23312329f
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-12-19 10:40:15 -0600

    added a space to concat

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -15,6 +15,6 @@ SELECT * FROM employees WHERE last_name LIKE '%q%'; -- 1,873 rows
 SELECT * FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya'; -- 709 rows
 SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya') AND gender = 'M'; -- 441 rows
 SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e' ORDER BY emp_no DESC; -- 30,723 rows
-SELECT CONCAT(first_name,last_name) FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e' ORDER BY emp_no DESC; -- 899 rows
+SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE 'e%' AND last_name LIKE '%e' ORDER BY emp_no DESC; -- 899 rows
 SELECT DATEDIFF(NOW(),hire_date) FROM employees WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31' AND birth_date LIKE '%12-25' ORDER BY birth_date, hire_date DESC; -- 362 rows
 SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name NOT LIKE '%qu%'; -- 547 rows
