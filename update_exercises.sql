f5ca5a002586b0d712b3e45f091912221bfff979
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-12-18 18:06:29 -0600

    completed updated_exercises

--- update_exercises.sql
+++ update_exercises.sql
@@ -1,11 +1,5 @@
 USE codeup_test_db;
 
-/*
-All albums in your table.
-All albums released before 1980
-All albums by Michael Jackson
- */
-
 SELECT 'ALL ALBUMS' AS '';
 SELECT * FROM albums;
 SELECT 'UPDATED SALES ------------' AS '';
@@ -14,10 +8,18 @@ SET sales = (sales * 10);
 SELECT * FROM albums;
 
 
-
-
 SELECT 'ALL ALBUMS BEFORE 1980' AS '';
 SELECT * FROM albums WHERE release_date < 1980;
+SELECT  'BACK 100 YRS ----------' AS '';
+UPDATE albums
+SET release_date = release_date - 100
+WHERE release_date < 1980;
+SELECT  * FROM albums WHERE release_date < 1980;
 
-SELECT 'ALL ALBUMS BY ''MICHAEL JACKSON''' AS '';
+SELECT 'ALL ALBUMS BY MICHAEL JACKSON' AS '';
 SELECT * FROM albums WHERE artist LIKE 'MICHAEL JACKSON%';
+SELECT 'CHANGE ''MICHAEL'' -> ''PETER'' ---------' AS '';
+UPDATE albums
+SET artist = 'PETER JACKSON'
+WHERE artist LIKE 'MICHAEL JACKSON%';
+SELECT * FROM albums WHERE artist LIKE 'PETER JACKSON%';
